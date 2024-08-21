afd729e909914a36f54fa727d607649aa8e79718
amr <amr514863@gmail.com>
2024-08-21 13:59:39 +0300

    changing vercel.json

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,7 @@
 import dotEnv from 'dotenv'
 import { connect } from "mongoose";
 dotEnv.config();
+console.log(process.env.DB_URI);
 const dbConn = connect(process.env.DB_URI)
 .then(() => {
   console.log("dataBase connected successfully...");
