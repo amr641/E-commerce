73e3d2cdee4295f48681c085d718f43ead34f9c5
amr <amr514863@gmail.com>
2024-09-07 14:58:06 +0300

    create webhook

--- index.js
+++ index.js
@@ -3,11 +3,38 @@ import dotEnv from 'dotenv';
 import dbConn from './src/config/dbConfig.js';
 import bootstrab from './src/bootstrab.js';
 import fs from 'fs';
-import cors from 'cors'
+import cors from 'cors';
+import { catchError } from './src/middlewares/catchErrors.js';
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPT_SECRET_KEY);
+
 const app = express();
-const port = process.env.PORT||8080;
+const port = process.env.PORT || 8080;
 // body-parser
-app.use(cors())
+app.use(cors());
+// stripe webhook
+
+app.post(
+  '/api/webhook',
+  express.raw({ type: 'application/json' }),
+  catchError((req, res) => {
+    const sig = req.headers['stripe-signature'].toString();
+
+    let event;
+
+    event = stripe.webhooks.constructEvent(req.body, sig, "whsec_wevqhq52rlCK216ws4v5Al4XGM69ikHf");
+
+    // Handle the event
+    let checkoutSessionCompleted ; 
+    if(event.type == "checkout.session.completed"){
+       checkoutSessionCompleted = event.data.object;
+    }
+ 
+
+    // Return a 200 res to acknowledge receipt of the event
+    res.json({details:checkoutSessionCompleted});
+  })
+);
 app.use(express.json());
 // app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
