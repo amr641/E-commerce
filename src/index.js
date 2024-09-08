28838212b078758867018ce7c7852c8226a468e2
amr <amr514863@gmail.com>
2024-09-08 20:12:52 +0300

    create webhook

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import fs from 'fs';
 import cors from 'cors';
 import { catchError } from './middlewares/catchErrors.js';
 import Stripe from 'stripe';
+import { Cart } from './models/cartModel.js';
+import showNotFound from '../utils/notFoundErrors.js';
+import { handelStockAndSold } from './middlewares/helpers/helpers.js';
+import User from './models/userModel.js';
 const stripe = new Stripe(process.env.STRIPT_SECRET_KEY);
 
 const app = express();
@@ -17,22 +21,44 @@ app.use(cors());
 app.post(
   '/api/webhook',
   express.raw({ type: 'application/json' }),
-  catchError((req, res) => {
+  catchError(async (req, res) => {
     const sig = req.headers['stripe-signature'].toString();
-
     let event;
-
-    event = stripe.webhooks.constructEvent(req.body, sig, "whsec_wevqhq52rlCK216ws4v5Al4XGM69ikHf");
-
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      'whsec_wevqhq52rlCK216ws4v5Al4XGM69ikHf'
+    );
     // Handle the event
-    let checkoutSessionCompleted ; 
-    if(event.type == "checkout.session.completed"){
-       checkoutSessionCompleted = event.data.object;
+    let checkoutSessionCompleted;
+    if (event.type == 'checkout.session.completed') {
+      checkoutSessionCompleted = event.data.object;
+      let user = await User.findOne({
+        email: checkoutSessionCompleted.customer_email,
+      });
+
+      let cart = await Cart.findById(
+        checkoutSessionCompleted.client_reference_id
+      );
+      if (!cart) return showNotFound(next, 'cart');
+      req.body.totalOrderPrice =
+        cart.totalPriceAfterDiscount || cart.totalCartPrice;
+      let order = new Order({
+        user: user._id,
+        orderItems: cart.cartItems,
+        shippingAddress: checkoutSessionCompleted.metadata,
+        totalOrderPrice: checkoutSessionCompleted.amount_total / 100,
+        paymentMethod:"card",
+        isPaid:true
+      });
+      await order.save();
+      handelStockAndSold(order);
+      await Cart.deleteOne({ _id: cart._id });
+      
     }
- 
 
     // Return a 200 res to acknowledge receipt of the event
-    res.json({details:checkoutSessionCompleted});
+    res.json({ details: checkoutSessionCompleted });
   })
 );
 app.use(express.json());
